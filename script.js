3f0e5f4a972b9590a48720955bc5e310356267e9
Artem <55661302@mail.ru>
2020-05-21 12:42:47 +0300

    dz2

--- script.js
+++ script.js
@@ -22,6 +22,36 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 }
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//     } else {
+//     console.log('error');
+//     i--;
+//     }
+// }
+
+// let i = 0;
+//     do {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его?', '');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         } else {
+//         console.log('error');
+//         i--;
+//         }
+//      }
+//      while(i < 2);
+
+
 
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
